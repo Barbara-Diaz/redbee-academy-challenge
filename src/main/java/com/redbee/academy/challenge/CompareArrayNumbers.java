f8158699545938ae1ba1f8c440858f19ec0ed3e3
Barbara-Diaz <81765187+Barbara-Diaz@users.noreply.github.com>
2021-06-17 18:18:54 -0300

    Update CompareArrayNumbers.java

--- src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
+++ src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
@@ -12,10 +12,26 @@ public class CompareArrayNumbers {
    * @param b - Another Integer Number
    * @return The result of a + b
    */
-  public static List<Integer> max(List<Integer> a, List<Integer> b) {
-    //TODO: Implement me
-    return null;
-  }
+    public static List<Integer> max(List<Integer> a, List<Integer> b) {
+        List<Integer> max=new ArrayList<>();
+       Integer maxList1=a.get(0);
+       Integer maxList2=b.get(0);
+        for (int i=0;i<a.size();i++) {
+            if ( a.get(i)>maxList1) {
+                maxList1=a.get(i);
+            }
+
+        }
+        for (int i=1;i<b.size();i++){
+            if (b.get(i)>maxList2){
+                maxList2=b.get(i);
+            }
+        }
+        max.add(maxList1);
+        max.add(maxList2);
+
+        return max;
+    }
 
 
 
